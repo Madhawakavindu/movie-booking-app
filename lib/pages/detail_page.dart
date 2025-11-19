706beffebf9199e81957a3a8cb2ea7142ce95669
Madhawakavindu <madhawakavindu7@gmail.com>
2025-11-19 23:32:32 +0530

    update

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -8,8 +9,18 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  List<String> getFormattedDates() {
+    final now = DateTime.now();
+    final formatter = DateFormat('EEE d');
+    return List.generate(7, (index) {
+      final date = now.add(Duration(days: index));
+      return formatter.format(date);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final dates = getFormattedDates();
     return Scaffold(
       body: Container(
         child: Stack(
@@ -29,20 +40,78 @@ class _DetailPageState extends State<DetailPage> {
               ),
               child: Icon(Icons.arrow_back, color: Colors.black),
             ),
-            Container(
-              margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height / 2.3,
-              ),
-              height: 200,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(left: 20.0, top: 20.0),
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 2.3,
+                  // left: 7.0,
+                  //right: 7.0,
+                ),
+
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Color(0xff1e232c),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Avengers Endgame",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Action / Sci-fi",
+                      style: TextStyle(
+                        color: const Color.fromARGB(189, 255, 255, 255),
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      "After Thanos wipes out half of all life, the universe falls into ruin. The remaining Avengers reunite with their allies to reverse his actions, restore balance, and face the ultimate battle—one that demands teamwork, sacrifice, and courage.",
+                      style: TextStyle(
+                        color: const Color.fromARGB(189, 255, 255, 255),
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Container(
+                      height: 60,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: dates.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            width: 100,
+                            margin: EdgeInsets.only(right: 20.0),
+                            decoration: BoxDecoration(color: Color(0xffeed51e)),
+                            child: Center(
+                              child: Text(
+                                dates[index],
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              child: Column(children: [Text("Avengers Endgame")]),
             ),
           ],
         ),

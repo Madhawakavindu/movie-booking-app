cabb19c5c97c2407320af5ab14010c74a3c48dfb
Madhawakavindu <madhawakavindu7@gmail.com>
2025-12-02 21:01:17 +0530

    update

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -8,9 +9,21 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  List<String> getFormattedDates() {
+    final now = DateTime.now();
+    final formatter = DateFormat('EEE d');
+    return List.generate(7, (index) {
+      final date = now.add(Duration(days: index));
+      return formatter.format(date);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final dates = getFormattedDates();
+
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Container(
         child: Stack(
           children: [
@@ -38,19 +51,19 @@ class _DetailPageState extends State<DetailPage> {
             ),
             Expanded(
               child: Container(
-                padding: EdgeInsets.only(left: 20.0, top: 20.0),
+                padding: EdgeInsets.only(left: 20.0, top: 20.0, right: 10.0),
                 margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height / 2.0,
-                  left: 8.0,
-                  right: 8.0,
+                  top: MediaQuery.of(context).size.height / 2.2,
+                  left: 5.0,
+                  right: 5.0,
                 ),
                 // height: 200,
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
                   color: Color(0xff1e232c),
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
                   ),
                 ),
                 child: Column(
@@ -63,6 +76,35 @@ class _DetailPageState extends State<DetailPage> {
                         fontSize: 26.0,
                         fontWeight: FontWeight.bold,
                       ),
+                    ),
+                    Text(
+                      "2019 ‧ Action/Sci-fi ‧ 3h 1m",
+                      style: TextStyle(
+                        color: const Color.fromARGB(174, 255, 255, 255),
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "The Avengers, shattered after Thanos wipes out half of all life, reunite with their remaining allies to restore the universe. They plan a dangerous time-travel mission to recover the Infinity Stones and undo the destruction. In a final epic battle against Thanos, they save the universe but suffer great sacrifices that change their team forever.",
+                      style: TextStyle(
+                        color: const Color.fromARGB(174, 255, 255, 255),
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    ListView.builder(
+                      itemCount: dates.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          decoration: BoxDecoration(color: Color(0xffeed51e)),
+                          child: Text(
+                            dates[index],
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        );
+                      },
                     ),
                   ],
                 ),
